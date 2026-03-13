020ad1faaa7c333a42b39c5c34c8184e6768b8d3
JuanCMH <jcmanchegoh@gmail.com>
2026-03-13 07:07:51 -0500

    [FIX] Mobile shell fixed

--- components/mobile-shell.tsx
+++ components/mobile-shell.tsx
@@ -5,7 +5,7 @@ interface MobileShellProps {
 export const MobileShell = ({ children }: MobileShellProps) => {
   return (
     <div className="flex items-center justify-center w-full h-screen bg-rose-100">
-      <div className="max-w-md w-full h-screen bg-white shadow-lg">
+      <div className="max-w-md w-full h-full bg-white shadow-lg py-16 md:py-0">
         {children}
       </div>
     </div>
